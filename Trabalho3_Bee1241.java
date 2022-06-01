86554231e233dbea8e66d2fcb14d140a705d7e5c
Vicente Hofmeister <vicentehofmeister@gmail.com>
2022-06-01 10:40:14 -0300

    Update Trabalho3_Bee1241.java

--- Trabalho3_Bee1241.java
+++ Trabalho3_Bee1241.java
@@ -10,14 +10,38 @@ import java.util.Scanner;
 public class Main{
 
       public static void ConfereEncaixe(int valorA, int valorB){
-            
+            int c = 1;
+            String a, b;
+
+            if (valorA >= valorB){
+
+                  a = "" + valorA;
+                  b = "" + valorB;
+
+                  for (int i = b.length(); i > 0; i--){
+
+                        if (a.charAt(a.length() - c) != b.charAt(b.length() - c)){
+                              System.out.println("nao encaixa");
+                              break;
+                        }
+                        else if (i == 1){
+                              System.out.println("encaixa");
+                        }
+
+                        c++;
+                  }
+
+
+            }
+            else{
+                  System.out.println("nao encaixa");
+            }
       }
 
       public static void main(String args[]){
             Scanner sc;
             int testes;
-            int valorA, valorB, c;
-            String a, b;
+            int valorA, valorB;
 
             sc = new Scanner(System.in);
             testes = sc.nextInt();
@@ -26,32 +50,8 @@ public class Main{
 
                   valorA = sc.nextInt();
                   valorB = sc.nextInt();
-                  a = "" + valorA;
-                  b = "" + valorB;
 
-                  if (valorA >= valorB){
-
-                        c = 1;
-
-                        for (int i = b.length(); i > 0; i--){
-
-                              if (a.charAt(a.length() - c) != b.charAt(b.length() - c)){
-                                    System.out.println("nao encaixa");
-                                    break;
-                              }
-                              else if (i == 1){
-                                    System.out.println("encaixa");
-                              }
-
-                              c++;
-                        }
-
-
-                  }
-                  else{
-                        System.out.println("nao encaixa");
-                  }
+                  ConfereEncaixe(valorA, valorB);
             }
-
       }
 }
